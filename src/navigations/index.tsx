61c5a7d35e5063568af7e983cff89026ed459775
HankTech <HankTech@gmail.com>
2022-11-29 15:58:22 -0400

    chat list

---
+++ src/navigations/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+//  render
+const App = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default App
